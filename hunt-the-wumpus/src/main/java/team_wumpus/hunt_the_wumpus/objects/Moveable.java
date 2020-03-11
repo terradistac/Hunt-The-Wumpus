4d32033a8198eaf9c1423b62a4822725e9cf8a76
Christina Terradista <terradistacc@gmail.com>
2020-03-11 14:03:59 +0000

    Adding moveable interface with Player and Wumpus

---
+++ hunt-the-wumpus/src/main/java/team_wumpus/hunt_the_wumpus/objects/Moveable.java
@@ -0,0 +1,8 @@
+package team_wumpus.hunt_the_wumpus.objects;
+
+public interface Moveable {
+	
+	public int getX();
+	public int getY();
+
+}
---
+++ hunt-the-wumpus/src/main/java/team_wumpus/hunt_the_wumpus/objects/Wumpus.java
@@ -0,0 +1,15 @@
+package team_wumpus.hunt_the_wumpus.objects;
+
+public class Wumpus implements Moveable {
+
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
