d4b7b0f105f79623a8641a1dcfce11d1f6624ae5
Christina Terradista <terradistacc@gmail.com>
2020-03-11 10:22:42 +0000

    Initial commit

---
+++ hunt-the-wumpus/src/main/java/team_wumpus/hunt_the_wumpus/App.java
@@ -0,0 +1,13 @@
+package team_wumpus.hunt_the_wumpus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
